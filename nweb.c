0351f49e50b6e314570f2882605b46355cac6a69
Ediber Montoya Moreira <ediber.cu@gmail.com>
2011-06-13 18:09:23 -0600

    Manejo de hilos

--- nweb.c
+++ nweb.c
@@ -10,80 +10,12 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <getopt.h>
+#include <pthread.h>
 
 #include "nweb.h"
 
 #define MAX_SOCKET_QUEUE 64
 #define DEFAULT_THREADS_COUNT 20
-
-/*
- * Structure to store not processed requests 
- */
-struct stack_item {
-  int socketfd;
-  int hit;
-};
-
-struct stack{
-  int top;
-  struct stack_item items[MAX_SOCKET_QUEUE];
-  pthread_mutex_t mutex;
-};
-
-void stack_init(struct stack * p_stack){
-  pthread_mutex_init(&((*p_stack).mutex));
-  (*p_stack).top = 0;
-}
-
-int stack_get_count(struct stack * p_stack){
-  int result = 0;
-  pthread_mutex_lock(&(*p_stack).mutex);
-
-  result = (*p_stack).top;
-
-  pthread_mutex_unlock(&(*p_stack).mutex);
-
-  return result;
-}
-
-int stack_pop(struct stack * p_stack, int * socketfd, int * hit){
-  int result = 0;
-  pthread_mutex_lock(&(*p_stack).mutex);
-
-  if ((*p_stack).top > 0){
-    (*p_stack).top--;
-
-    (*socketfd) = (*p_stack).items[(*p_stack).top].socketfd;
-    (*hit) = (*p_stack).items[(*p_stack).top].hit;
-
-    result = 1;
-  }
-
-  pthread_mutex_unlock(&(*p_stack).mutex);
-
-  return result;
-}
-
-int stack_push(struct stack * p_stack, int socketfd, int hit){
-  int result = 0;
-  pthread_mutex_lock(&(*p_stack).mutex);
-
-  if ((*p_stack).top < MAX_SOCKET_QUEUE -1){
-    (*p_stack).top++;
-
-    (*p_stack).items[(*p_stack).top].socketfd = socketfd;
-    (*p_stack).items[(*p_stack).top].hit = hit;
-
-    result = 1;
-  }
-
-  pthread_mutex_unlock(&(*p_stack).mutex);
-
-  return result;
-}
-
-
-struct stack connections;
 
 void log(int type, char *s1, char *s2, int num)
 {
@@ -108,6 +40,83 @@ void log(int type, char *s1, char *s2, int num)
 	if(type == ERROR) exit(3);
 }
 
+
+/*
+ * Structure to store not processed requests 
+ */
+struct stack_item {
+  int socketfd;
+  int hit;
+};
+
+struct stack{
+  int top;
+  struct stack_item items[MAX_SOCKET_QUEUE];
+  pthread_mutex_t mutex;
+};
+
+void stack_init(struct stack * p_stack){
+  pthread_mutex_init(&((*p_stack).mutex), NULL);
+  (*p_stack).top = 0;
+}
+
+int stack_get_count(struct stack * p_stack){
+  int result = 0;
+  pthread_mutex_lock(&(*p_stack).mutex);
+
+  result = (*p_stack).top;
+
+  pthread_mutex_unlock(&(*p_stack).mutex);
+
+  return result;
+}
+
+int stack_pop(struct stack * p_stack, int * socketfd, int * hit){
+  int result = 0;
+  pthread_mutex_lock(&(*p_stack).mutex);
+
+  if ((*p_stack).top > 0){
+    (*p_stack).top--;
+
+    (*socketfd) = (*p_stack).items[(*p_stack).top].socketfd;
+    (*hit) = (*p_stack).items[(*p_stack).top].hit;
+
+    log(LOG, "Stack:", "pop", *hit);
+
+    result = 1;
+  }
+
+  pthread_mutex_unlock(&(*p_stack).mutex);
+
+  return result;
+}
+
+int stack_push(struct stack * p_stack, int socketfd, int hit){
+  int result = 0;
+  log(LOG, "Stack:", "push", hit);
+  pthread_mutex_lock(&(*p_stack).mutex);
+
+  if ((*p_stack).top < MAX_SOCKET_QUEUE -1){
+    (*p_stack).items[(*p_stack).top].socketfd = socketfd;
+    (*p_stack).items[(*p_stack).top].hit = hit;
+
+    (*p_stack).top++;
+
+    result = 1;
+  }
+
+  pthread_mutex_unlock(&(*p_stack).mutex);
+
+  return result;
+}
+
+
+struct stack connections;
+
+pthread_mutex_t nweb_mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t nweb_cond = PTHREAD_COND_INITIALIZER;
+
+
 /* this is a child web server process, so we can exit on errors */
 void * web(void * param)
 {
@@ -115,67 +124,97 @@ void * web(void * param)
 	long i, ret;
 	char * fstr;
 	static char buffer[BUFSIZE+1]; /* static so zero filled */
+  int proceed = 0;
 
-  stack_pop(&connections, &fd, &hit);
+  while(1){
 
-	ret =read(fd,buffer,BUFSIZE); 	/* read Web request in one go */
-	if(ret == 0 || ret == -1) {	/* read failure stop now */
-		log(SORRY,"failed to read browser request","",fd);
-	}
-	if(ret > 0 && ret < BUFSIZE)	/* return code is valid chars */
-		buffer[ret]=0;		/* terminate the buffer */
-	else buffer[0]=0;
+    pthread_mutex_lock(&nweb_mutex);
 
-	for(i=0;i<ret;i++)	/* remove CF and LF characters */
-		if(buffer[i] == '\r' || buffer[i] == '\n')
-			buffer[i]='*';
-	log(LOG,"request",buffer,hit);
+    proceed = 0;
 
-	if( strncmp(buffer,"GET ",4) && strncmp(buffer,"get ",4) )
-		log(SORRY,"Only simple GET operation supported",buffer,fd);
+    if(!(proceed = stack_pop(&connections, &fd, &hit))){ // if there is no pending connections to handle
+      pthread_cond_wait(&nweb_cond, &nweb_mutex); // lock waiting for condition
 
-	for(i=4;i<BUFSIZE;i++) { /* null terminate after the second space to ignore extra stuff */
-		if(buffer[i] == ' ') { /* string is "GET URL " +lots of other stuff */
-			buffer[i] = 0;
-			break;
-		}
-	}
+      proceed = stack_pop(&connections, &fd, &hit); // Obtain the connection info
+      
+      if(proceed){
+        ret =read(fd,buffer,BUFSIZE); 	/* read Web request in one go */
+      }
+    }
+    else
+      ret =read(fd,buffer,BUFSIZE); 	/* read Web request in one go */
+    
+    // release the mutex as soon as the data was loaded into local buffer
+    pthread_mutex_unlock(&nweb_mutex);
 
-	for(j=0;j<i-1;j++) 	/* check for illegal parent directory use .. */
-		if(buffer[j] == '.' && buffer[j+1] == '.')
-			log(SORRY,"Parent directory (..) path names not supported",buffer,fd);
+    /*
+     * The thread will continue running while there exists any pending connections in stack
+     * this behaviour ensure that if the working threads are all working when new connections
+     * arrives, then any running thread could handle it.
+     */
+    if (proceed){
 
-	if( !strncmp(&buffer[0],"GET /\0",6) || !strncmp(&buffer[0],"get /\0",6) ) /* convert no filename to index file */
-		(void)strcpy(buffer,"GET /index.html");
+      if(ret == 0 || ret == -1) {	/* read failure stop now */
+        log(SORRY,"failed to read browser request","",fd);
+      }
+      if(ret > 0 && ret < BUFSIZE)	/* return code is valid chars */
+        buffer[ret]=0;		/* terminate the buffer */
+      else buffer[0]=0;
 
-	/* work out the file type and check we support it */
-	buflen=strlen(buffer);
-	fstr = (char *)0;
-	for(i=0;extensions[i].ext != 0;i++) {
-		len = strlen(extensions[i].ext);
-		if( !strncmp(&buffer[buflen-len], extensions[i].ext, len)) {
-			fstr =extensions[i].filetype;
-			break;
-		}
-	}
-	if(fstr == 0) log(SORRY,"file extension type not supported",buffer,fd);
+      for(i=0;i<ret;i++)	/* remove CF and LF characters */
+        if(buffer[i] == '\r' || buffer[i] == '\n')
+          buffer[i]='*';
+      log(LOG,"request",buffer,hit);
 
-	if(( file_fd = open(&buffer[5],O_RDONLY)) == -1) /* open the file for reading */
-		log(SORRY, "failed to open file",&buffer[5],fd);
+      if( strncmp(buffer,"GET ",4) && strncmp(buffer,"get ",4) )
+        log(SORRY,"Only simple GET operation supported",buffer,fd);
 
-	log(LOG,"SEND",&buffer[5],hit);
+      for(i=4;i<BUFSIZE;i++) { /* null terminate after the second space to ignore extra stuff */
+        if(buffer[i] == ' ') { /* string is "GET URL " +lots of other stuff */
+          buffer[i] = 0;
+          break;
+        }
+      }
 
-	(void)sprintf(buffer,"HTTP/1.0 200 OK\r\nContent-Type: %s\r\n\r\n", fstr);
-	(void)write(fd,buffer,strlen(buffer));
+      for(j=0;j<i-1;j++) 	/* check for illegal parent directory use .. */
+        if(buffer[j] == '.' && buffer[j+1] == '.')
+          log(SORRY,"Parent directory (..) path names not supported",buffer,fd);
 
-	/* send file in 8KB block - last block may be smaller */
-	while (	(ret = read(file_fd, buffer, BUFSIZE)) > 0 ) {
-		(void)write(fd,buffer,ret);
-	}
+      if( !strncmp(&buffer[0],"GET /\0",6) || !strncmp(&buffer[0],"get /\0",6) ) /* convert no filename to index file */
+        (void)strcpy(buffer,"GET /index.html");
+
+      /* work out the file type and check we support it */
+      buflen=strlen(buffer);
+      fstr = (char *)0;
+      for(i=0;extensions[i].ext != 0;i++) {
+        len = strlen(extensions[i].ext);
+        if( !strncmp(&buffer[buflen-len], extensions[i].ext, len)) {
+          fstr =extensions[i].filetype;
+          break;
+        }
+      }
+      if(fstr == 0) log(SORRY,"file extension type not supported",buffer,fd);
+
+      if(( file_fd = open(&buffer[5],O_RDONLY)) == -1) /* open the file for reading */
+        log(SORRY, "failed to open file",&buffer[5],fd);
+
+      log(LOG,"SEND",&buffer[5],hit);
+
+      (void)sprintf(buffer,"HTTP/1.0 200 OK\r\nContent-Type: %s\r\n\r\n", fstr);
+      (void)write(fd,buffer,strlen(buffer));
+
+      /* send file in 8KB block - last block may be smaller */
+      while (	(ret = read(file_fd, buffer, BUFSIZE)) > 0 ) {
+        (void)write(fd,buffer,ret);
+      }
+
+      (void)close(fd);
 #ifdef LINUX
-	sleep(1);	/* to allow socket to drain */
+      sleep(1);	/* to allow socket to drain */
 #endif
-	//exit(1);
+      //exit(1);
+    }
+  }
 }
 
 
@@ -185,6 +224,7 @@ int main(int argc, char **argv)
 	size_t length;
 	static struct sockaddr_in cli_addr; /* static = initialised to zeros */
 	static struct sockaddr_in serv_addr; /* static = initialised to zeros */
+  pthread_t nweb_thread;
 
   threads_count = DEFAULT_THREADS_COUNT;
 
@@ -210,7 +250,9 @@ int main(int argc, char **argv)
 	"\tnweb only servers out file/web pages with extensions named below\n"
 	"\t and only from the named directory or its sub-directories.\n"
 	"\tThere is no fancy features = safe and secure.\n\n"
-	"\tExample: nweb 8181 /home/nwebdir &\n\n"
+	"\t\tExample: nweb 8181 /home/nwebdir &\n\n"
+	"\tor you can indicate the number of working threads to handle requests:\n\n"
+	"\t\tExample: nweb -t 20 8181 /home/nwebdir &\n\n"
 	"\tOnly Supports:");
 		for(i=0;extensions[i].ext != 0;i++)
 			(void)printf(" %s",extensions[i].ext);
@@ -262,6 +304,12 @@ int main(int argc, char **argv)
   // initialize connection stack
   stack_init(&connections);
 
+  log(LOG, "Stack:", "count", stack_get_count(&connections));
+
+  for (i=0; i < threads_count; i++){
+     pthread_create(&nweb_thread, NULL, web, NULL);
+  }
+
 	for(hit=1; 1; hit++) {
 		length = sizeof(cli_addr);
 
@@ -270,10 +318,19 @@ int main(int argc, char **argv)
 		if((socketfd = accept(listenfd, (struct sockaddr *)&cli_addr, &length)) < 0)
 			log(ERROR,"system call","accept",0);
 
-    stack_push(&connections, socketfd, hit);
+    log(LOG, "New connection:", "Id", hit);
 
-    web(NULL); 
+    if(stack_push(&connections, socketfd, hit)){
 
-    (void)close(socketfd);
+      pthread_mutex_lock(&nweb_mutex);
+    
+      pthread_cond_signal(&nweb_cond);
+
+      //web(NULL); 
+    
+      pthread_mutex_unlock(&nweb_mutex);
+    }
+    else
+      (void)close(socketfd);
 	}
 }
